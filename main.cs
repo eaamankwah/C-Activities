b741c2bce19e6601d363387f0d76a97bc52d19c5
Edward Amankwah <eaakwasi@yahoo.ca>
2022-06-30 05:01:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+  class Program
+  {
+      static void Main(string[] args)
+      {
+        Console.WriteLine("Hello Edward"); //a semi-colon indicates the end of a line of code. This line writes to the screen
+            /*This is a multi line comment
+             * This is another line in that comment. We only need closing tags for multi-line comments, not single line*/
+           
+            Console.Read(); //good to get in the habit of having Console.Read() at the end of your program, just to make sure your program doesn't stop and not allow you to see what you entered; Console.Read() waits for user input*/
+  
+      }
+  }
+
